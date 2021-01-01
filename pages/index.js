108c961642bc1044d0f4fed7d490b16c067b5df9
Nestor Diaz <ndiaz@salesforce.com>
2021-01-01 13:11:05 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,39 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { GithubIcon } from '../components/icons/GithubIcon';
+import { TwitterIcon } from '../components/icons/TwitterIcon';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="font-sans antialiased">
       <Head>
-        <title>Create Next App</title>
+        <title>Néstor Diaz</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="max-w-5xl mx-auto h-56 p-4">
+        <figure className="flex items-center">
+          <img class="w-32 h-32 rounded-full" src="/coldhams.jpeg" alt="" width="72" height="72" />
+          <div class="pt-6 md:p-8 text-center md:text-left">
+            <p class="text-2xl">Néstor Diaz</p>
+            <figcaption class="font-medium">
+              <div class="text-gray-500">Frontend Developer</div>
+              <ul className="w-24 text-gray-600 pt-5">
+                <li className="pb-3">
+                  <a className="flex hover:text-gray-900" href="https://github.com/ndiaz-candido" target="_blank">
+                    <GithubIcon />
+                    <span className="ml-2">Github</span>
+                  </a>
+                </li>
+                <li>
+                  <a className="flex hover:text-gray-900" href="https://twitter.com/nestor_diazl" target="_blank">
+                    <TwitterIcon />
+                    <span className="ml-2">Twitter</span>
+                  </a>
+                </li>
+              </ul>
+            </figcaption>
+          </div>
+        </figure>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
