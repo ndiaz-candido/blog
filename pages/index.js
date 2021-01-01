c3cd2c8a32ec49df39e7510dd75cc6f12c81d646
Nestor Diaz <ndiaz@ebi.ac.uk>
2021-01-01 22:56:24 +0000

    Improve responsiveness

--- pages/index.js
+++ pages/index.js
@@ -10,26 +10,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="max-w-5xl mx-auto h-56 p-4">
-        <figure className="flex items-center">
-          <img class="w-32 h-32 rounded-full" src="/coldhams.jpeg" alt="" width="72" height="72" />
-          <div class="pt-6 md:p-8 text-center md:text-left">
-            <p class="text-2xl">Néstor Diaz</p>
-            <figcaption class="font-medium">
-              <div class="text-gray-500">Frontend Developer</div>
-              <ul className="w-24 text-gray-600 pt-5">
-                <li className="pb-3">
-                  <a className="flex hover:text-gray-900" href="https://github.com/nestor-diaz" target="_blank">
-                    <GithubIcon />
-                    <span className="ml-2">Github</span>
-                  </a>
-                </li>
-                <li>
-                  <a className="flex hover:text-gray-900" href="https://twitter.com/nestor_diazl" target="_blank">
-                    <TwitterIcon />
-                    <span className="ml-2">Twitter</span>
-                  </a>
-                </li>
-              </ul>
+        <figure className="flex flex-col items-center md:flex-row">
+          <img className="w-32 h-32 rounded-full" src="/coldhams.jpeg" alt="" width="72" height="72" />
+          <div className="text-center md:pl-8 md:text-left">
+            <p className="text-2xl">Néstor Diaz</p>
+            <figcaption className="font-medium">
+              <div className="text-gray-500">Frontend Developer</div>
+              <div className="flex flex-col items-center md:flex-row text-gray-600 pt-5">
+                <a className="flex hover:text-gray-900 py-2" href="https://github.com/nestor-diaz" target="_blank">
+                  <GithubIcon />
+                  <span className="ml-2">Github</span>
+                </a>
+                <a
+                  className="flex hover:text-gray-900 py-2 md:pl-4"
+                  href="https://twitter.com/nestor_diazl"
+                  target="_blank"
+                >
+                  <TwitterIcon />
+                  <span className="ml-2">Twitter</span>
+                </a>
+              </div>
             </figcaption>
           </div>
         </figure>
